b77e90359e7458e54db3d4067e809355cddbdf22
Ty1Ty1 <145836936+Ty1Ty1@users.noreply.github.com>
2025-05-08 02:42:13 +0200

    Update __init__.py

--- HotelGuruApp/app/blueprints/admin/__init__.py
+++ HotelGuruApp/app/blueprints/admin/__init__.py
@@ -1,5 +1,6 @@
+#from flask import Blueprint
 from apiflask import APIBlueprint
 
-bp = APIBlueprint('admin', __name__, tag="Admin")
+bp = APIBlueprint('admin', __name__, tag="admin")
 
 from app.blueprints.admin import routes

d65c87a951042d87b8b5a9cdbe5c8d1548d9bba6
Ty1Ty1 <145836936+Ty1Ty1@users.noreply.github.com>
2025-05-08 02:37:56 +0200

    Update __init__.py

--- HotelGuruApp/app/blueprints/__init__.py
+++ HotelGuruApp/app/blueprints/__init__.py
@@ -1,16 +1,49 @@
+#from flask import Blueprint
 from apiflask import APIBlueprint
+bp = APIBlueprint('main', __name__, tag="default")
 
-bp = APIBlueprint('main', __name__, tag="main")
+from app.extensions import auth
+from flask import current_app
+from authlib.jose import jwt
+from datetime import datetime
+from apiflask import HTTPError
 
-from app.blueprints import bp
+@auth.verify_token
+def verify_token(token):
+    try:
+        data = jwt.decode(
+            token.encode('ascii'),
+           
+            current_app.config['SECRET_KEY'],
+        )
+        if data["exp"] < int(datetime.now().timestamp()):
+            return None
+        return data
+    except Exception as ex:
+        return None
 
-@bp.route('/')
-def index():
-    return 'This is The Main Blueprint'
+def role_required(roles):
+    def wrapper(fn):
+        def decorated_function(*args, **kwargs):
+            user_roles = [item["name"] for item in auth.current_user.get("roles")]
+            for role in roles:
+                if role in user_roles:
+                    return fn(*args, **kwargs)        
+            raise HTTPError(message="Access denied", status_code=403)
+        return decorated_function
+    return wrapper
 
 
-#registrate blueprints
 from app.blueprints.user import bp as bp_user
 bp.register_blueprint(bp_user, url_prefix='/user')
+
+from app.blueprints.admin import bp as bp_admin
+bp.register_blueprint(bp_admin, url_prefix='/admin')
+
+from app.blueprints.guest import bp as bp_guest
+bp.register_blueprint(bp_guest, url_prefix='/guest')
+
+from app.blueprints.receptionist import bp as bp_receptionist
+bp.register_blueprint(bp_receptionist, url_prefix='/receptionist')
 
 from app.models import *

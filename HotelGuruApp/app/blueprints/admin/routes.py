2be240a1e43bdbf879f8e1ac0688d01ea535c0c9
Ty1Ty1 <145836936+Ty1Ty1@users.noreply.github.com>
2025-05-08 02:41:57 +0200

    Update routes.py

--- HotelGuruApp/app/blueprints/admin/routes.py
+++ HotelGuruApp/app/blueprints/admin/routes.py
@@ -3,56 +3,60 @@ from app.blueprints.admin.schemas import (
     RoomCreateUpdateSchema, RoomAdminResponseSchema, RoomStatusUpdateSchema
 )
 from app.blueprints.admin.service import AdminService
-from apiflask import HTTPError, input, output, abort
+from apiflask import HTTPError
 from apiflask.fields import Integer
 
+@bp.route('/')
+def index():
+    return 'This is The Admin Blueprint'
+
 @bp.get('/rooms')
-@output(RoomAdminResponseSchema(many=True))
+@bp.output(RoomAdminResponseSchema(many=True))
 def admin_list_rooms():
     success, response = AdminService.list_all_rooms()
     if success:
         return response, 200
-    abort(400, message=response)
+    raise HTTPError(message=response, status_code=400)
 
 @bp.post('/rooms')
-@input(RoomCreateUpdateSchema)
-@output(RoomAdminResponseSchema, status_code=201)
+@bp.input(RoomCreateUpdateSchema)
+@bp.output(RoomAdminResponseSchema, status_code=201)
 def admin_create_room(data):
     success, response = AdminService.create_room(data)
     if success:
         return response, 201
-    abort(400, message=response) 
+    raise HTTPError(message=response, status_code=400)
 
 @bp.get('/rooms/<int:room_id>')
-@output(RoomAdminResponseSchema)
+@bp.output(RoomAdminResponseSchema)
 def admin_get_room(room_id):
     success, response = AdminService.get_room_details(room_id)
     if success:
         return response, 200
-    abort(404, message=response) 
+    raise HTTPError(message=response, status_code=400) 
 
 @bp.put('/rooms/<int:room_id>')
-@input(RoomCreateUpdateSchema)
-@output(RoomAdminResponseSchema)
+@bp.input(RoomCreateUpdateSchema)
+@bp.output(RoomAdminResponseSchema)
 def admin_update_room(room_id, data):
     success, response = AdminService.update_room(room_id, data)
     if success:
         return response, 200
-    abort(400, message=response) 
+    raise HTTPError(message=response, status_code=400)
 
 @bp.delete('/rooms/<int:room_id>')
-@output({}, status_code=200)
+@bp.output({}, status_code=200)
 def admin_delete_room(room_id):
     success, response = AdminService.delete_room(room_id)
     if success:
         return response, 200
-    abort(400, message=response) 
+    raise HTTPError(message=response, status_code=400) 
 
 @bp.put('/rooms/<int:room_id>/status')
-@input(RoomStatusUpdateSchema)
-@output(RoomAdminResponseSchema) 
+@bp.input(RoomStatusUpdateSchema)
+@bp.output(RoomAdminResponseSchema) 
 def admin_update_room_status(room_id, data):
     success, response = AdminService.update_room_status(room_id, data['status'])
     if success:
         return response, 200
-    abort(400, message=response) 
+    raise HTTPError(message=response, status_code=400) 

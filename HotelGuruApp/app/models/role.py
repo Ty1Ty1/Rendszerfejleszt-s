cb17c964c506a1a55c040e7fb1bbba63d5326710
Ty1Ty1 <145836936+Ty1Ty1@users.noreply.github.com>
2025-05-08 02:36:47 +0200

    Update role.py

--- HotelGuruApp/app/models/role.py
+++ HotelGuruApp/app/models/role.py
@@ -1,24 +1,22 @@
-# app/models/role.py
+from __future__ import annotations
+
 from app.extensions import db
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.types import String
 from typing import List
+from sqlalchemy.types import String
+
+from app.models.user import UserRole
 
 from typing import List, TYPE_CHECKING
 if TYPE_CHECKING:
     from app.models.user import User
 
-from app.models.association import UserRole
-
 class Role(db.Model):
     __tablename__ = "roles"
-
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-
-    users: Mapped[List["User"]] = relationship(
-        secondary=UserRole, back_populates="roles"
-    )
+    name: Mapped[str] = mapped_column(String(30)) 
+    users : Mapped[List["User"]] = relationship(secondary=UserRole, back_populates="roles")
+    
 
     def __repr__(self) -> str:
         return f"Role(id={self.id!r}, name={self.name!s})"

b341d0bd452baee7dbd3ef4c61c724e120bfd4e9
Ty1Ty1 <145836936+Ty1Ty1@users.noreply.github.com>
2025-05-08 02:37:08 +0200

    Delete HotelGuruApp/app/models/association.py

--- HotelGuruApp/app/models/association.py
+++
@@ -1,9 +0,0 @@
-from app.extensions import Base
-from sqlalchemy import Column, ForeignKey, Table
-
-UserRole = Table(
-    "userroles",
-    Base.metadata,
-    Column("user_id", ForeignKey("users.id")),
-    Column("role_id", ForeignKey("roles.id"))
-)

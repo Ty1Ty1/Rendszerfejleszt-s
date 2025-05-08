ef68cd4470dbc26469da9faeb9be4d3b5881da00
Ty1Ty1 <145836936+Ty1Ty1@users.noreply.github.com>
2025-05-08 02:39:55 +0200

    Update routes.py

--- HotelGuruApp/app/blueprints/receptionist/routes.py
+++ HotelGuruApp/app/blueprints/receptionist/routes.py
@@ -3,48 +3,46 @@ from app.blueprints.receptionist.schemas import (
     ReceptionistBookingResponseSchema, BillResponseSchema
 )
 from app.blueprints.receptionist.service import ReceptionistService
-from apiflask import HTTPError, output, abort
+from apiflask import HTTPError
 from apiflask.fields import Integer
 
-# @auth_required # Recepciós szerepkör ellenõrzése szükséges lehet
-
 @bp.get('/bookings/pending')
-@output(ReceptionistBookingResponseSchema(many=True))
+@bp.output(ReceptionistBookingResponseSchema(many=True))
 def receptionist_list_pending_bookings():
     success, response = ReceptionistService.list_pending_bookings()
     if success:
         return response, 200
-    abort(400, message=response)
+    raise HTTPError(message=response, status_code=400)
 
 
 @bp.put('/bookings/<int:booking_id>/confirm')
-@output(ReceptionistBookingResponseSchema)
+@bp.output(ReceptionistBookingResponseSchema)
 def receptionist_confirm_booking(booking_id):
     success, response = ReceptionistService.confirm_booking(booking_id)
     if success:
         return response, 200
-    abort(400, message=response)
+    raise HTTPError(message=response, status_code=400)
 
 @bp.put('/bookings/<int:booking_id>/check-in')
-@output(ReceptionistBookingResponseSchema)
+@bp.output(ReceptionistBookingResponseSchema)
 def receptionist_check_in(booking_id):
     success, response = ReceptionistService.check_in(booking_id)
     if success:
         return response, 200
-    abort(400, message=response) 
+    raise HTTPError(message=response, status_code=400)
 
 @bp.put('/bookings/<int:booking_id>/check-out')
-@output(BillResponseSchema) 
+@bp.output(BillResponseSchema) 
 def receptionist_check_out(booking_id):
     success, response = ReceptionistService.check_out_and_generate_bill(booking_id)
     if success:
         return response, 200
-    abort(400, message=response) 
+    raise HTTPError(message=response, status_code=400) 
 
 @bp.get('/bookings/<int:booking_id>/bill')
-@output(BillResponseSchema)
+@bp.output(BillResponseSchema)
 def receptionist_get_bill(booking_id):
     success, response = ReceptionistService.get_bill(booking_id)
     if success:
         return response, 200
-    abort(404, message=response) 
+    raise HTTPError(message=response, status_code=400)

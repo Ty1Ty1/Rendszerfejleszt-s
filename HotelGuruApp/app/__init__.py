5ba7bdd7791994d99fd7a9b7f06681d4dd36b6c5
Ty1Ty1 <145836936+Ty1Ty1@users.noreply.github.com>
2025-05-08 02:34:27 +0200

    Update __init__.py

--- HotelGuruApp/app/__init__.py
+++ HotelGuruApp/app/__init__.py
@@ -1,23 +1,14 @@
-from apiflask import APIFlask
+from apiflask import APIFlask, HTTPTokenAuth
+from flask import Flask
 from config import Config
 from app.extensions import db
-
+from app.models import *
+from app.blueprints.main import bp as main_bp
 
 def create_app(config_class=Config):
-    #app = Flask(__name__)
-    app = APIFlask(__name__, json_errors = True, 
-               title="HotelGuru API",
-               docs_path="/swagger")
+    app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialize Flask extensions here
-    db.init_app(app)
-    
-    from flask_migrate import Migrate
-    migrate = Migrate(app, db, render_as_batch=True)
-    
-    # Register blueprints here
-    from app.blueprints import bp as main_bp
-    app.register_blueprint(main_bp, url_prefix="/api")
+    app.register_blueprint(main_bp)
 
     return app

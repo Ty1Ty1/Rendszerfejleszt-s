e031b1d0afc7db71ed529b9a21ca897294945361
Ty1Ty1 <145836936+Ty1Ty1@users.noreply.github.com>
2025-05-13 20:03:10 +0200

    Update __init__.py

--- HotelGuruApp/app/blueprints/main/__init__.py
+++ HotelGuruApp/app/blueprints/main/__init__.py
@@ -1,7 +1,19 @@
+from apiflask import APIBlueprint
 
-from flask import Blueprint
+bp = APIBlueprint('main', __name__)
 
-bp = Blueprint('main', __name__)
+
+from app.blueprints.admin import bp as admin_bp
+bp.register_blueprint(admin_bp)
+
+from app.blueprints.guest import bp as guest_bp
+bp.register_blueprint(guest_bp)
+
+from app.blueprints.receptionist import bp as receptionist_bp
+bp.register_blueprint(receptionist_bp)
+
+from app.blueprints.user import bp as user_bp
+bp.register_blueprint(user_bp)
 
 # Importáljuk a routes.py-t
 from app.blueprints.main import routes
